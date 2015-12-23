7f617732ef3abe887e8eab9a09da0520ad0c7142
Adan Orta <adanorta@Adans-MacBook-Pro.local>
2015-12-23 11:18:00 -0600

    added name field

--- server/api/project/project.model.js
+++ server/api/project/project.model.js
@@ -3,9 +3,10 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var ProjectSchema = new mongoose.Schema({
+  name: String,
+  description: String,
   created_by: String,
   investigator: String,
-  description: String,
   date_created: Date,
   date_completed: Date,
 });
