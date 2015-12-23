87e107b70ef79c59335e83459f727c1315c59b4e
Adan Orta <adanorta@Adans-MacBook-Pro.local>
2015-12-23 11:09:29 -0600

    added sample model

--- server/api/project/project.model.js
+++ server/api/project/project.model.js
@@ -3,7 +3,9 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var ProjectSchema = new mongoose.Schema({
-  name: String,
+  created_by: String,
+  investigator: String,
+  description: String,
   date_created: Date,
   date_completed: Date,
 });
--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,9 @@ import path from 'path';
 export default function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+  app.use('/api/projects', require('./api/project'));
+  
+  
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
